d7c50147296931d1dd0e211c3e286ff6b5185b75
Oke <oakie.lukas@gmail.com>
2023-02-13 12:27:22 +0100

    Okes part

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,11 +1,20 @@
 const arrayPerson=[];
 const arrayVerb=[];
-const arrayObject=[];
+const arrayObject=['the universe', 'the ocean', 'pollution', 'clouds', 'to go vegan', 'in the air', 'you', 'everyone', 'bees', 'Nashville, TN'];
 
 const randomPerson:'';
 const randomVerb:'';
-const randomObject='';
-
-function mix () {
-    
+const randomObject = () => {
+    return arrayObject[Math.floor(Math.random()*9)];
 }
+
+/* function mix () {
+    
+} */
+
+
+
+
+
+
+console.log(randomObject());
